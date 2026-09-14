79b975a82edd1cee2a889be0976a7a6959af74b1
jeevan singh <mantupanora@gmail.com>
2026-09-14 20:17:47 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.31.197:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
 });
 
 export default API;
